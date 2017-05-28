e884c442362784367baef9ce75fa583e492821fd
Paul Liu <pstar.liu@gmail.com>
2017-05-28 16:44:20 +1200

    add code to handle empty string in the list

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -9,12 +9,15 @@ LunchCheckController.$inject = ['$scope'];
 function LunchCheckController($scope) {
     $scope.listOfLunch = '';
     $scope.message = '';
+    $scope.lists = null;
 
     $scope.check = function () {
         if ($scope.listOfLunch.length === 0) {
             $scope.message = 'Please enter data first.';
         } else {
-            var itemCount = $scope.listOfLunch.split(',').length;
+            $scope.lists = $scope.listOfLunch.split(',').filter(s => s.trim().length > 0);
+            var itemCount = $scope.lists.length;
+            
             if (itemCount > 3) {
                 $scope.message = 'Too much!';
             } else {
