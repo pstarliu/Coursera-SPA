e16b31882d4998d9ebcc6a536dbcb5c8dc3c5855
Paul Liu <pstar.liu@gmail.com>
2017-05-28 17:29:56 +1200

    add different color styles for invalid input and valid input

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -10,11 +10,25 @@ function LunchCheckController($scope) {
     $scope.listOfLunch = '';
     $scope.message = '';
     $scope.lists = null;
+    $scope.messageStyle = {};
+    $scope.inputBoxSytle = {};
+
+    $scope.turnGreen = function () {
+        $scope.messageStyle.colorClass = "green";
+        $scope.inputBoxSytle.borderClass = 'green-border';
+    }
+
+    $scope.turnRed = function () {
+        $scope.messageStyle.colorClass = "red";
+        $scope.inputBoxSytle.borderClass = 'red-border';
+    }
 
     $scope.check = function () {
         if ($scope.listOfLunch.length === 0) {
             $scope.message = 'Please enter data first.';
+            $scope.turnRed();
         } else {
+            $scope.turnGreen();
             $scope.lists = $scope.listOfLunch.split(',').filter(s => s.trim().length > 0);
             var itemCount = $scope.lists.length;
             
