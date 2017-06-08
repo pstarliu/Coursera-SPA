026b732baf09f4ef3be93e11bcff5fabb32b445b
Paul Liu <pstar.liu@gmail.com>
2017-06-09 04:46:54 +1200

    currently the controller found is always null, not exactlly know how to fix that yet

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -1,32 +1,57 @@
 (function () {
-'use strict';
+    'use strict';
 
-angular.module('NarrowItDownApp', [])
-.controller('NarrowItDownController')
-.service('MenuSearchService', MenuSearchService)
-.constant('ApiBasePath', 'https://davids-restaurant.herokuapp.com/');
-.directive('foundItems', FoundItem);
+    angular.module('NarrowItDownApp', [])
+        .controller('NarrowItDownController', NarrowItDownController)
+        .service('MenuSearchService', MenuSearchService)
+        .constant('ApiBasePath', 'https://davids-restaurant.herokuapp.com/')
+        .directive('foundItems', FoundItems);
 
+    function FoundItems() {
+        var ddo = {
+            templateUrl: 'foundList.html',
+            scope: {
+                items: '<',
+                onRemove: '&'
+            },
+            controller: NarrowItDownController,
+            controllerAs: 'list',
+            bindToController: true
+        };
 
-NarrowItDownController.$inject = ['MenuSearchService'];
-function NarrowItDownController(MenuSearchService) {
-
-}
-
-MenuSearchService.$inject = ['$http', 'ApiBasePath'];
-function MenuSearchService($http, ApiBasePath) {
-    var service = this;
-
-    service.getMatchedMenuItems = function (searchTerm) {
-        var respose = $http(...).then(function (result) {
-        }
-        return response;
-    // process result and only keep items that match
-    var foundItems...
-
-    // return processed items
-    return foundItems;
-});
+        return ddo;
     }
-}
+
+    NarrowItDownController.$inject = ['MenuSearchService'];
+    function NarrowItDownController(MenuSearchService) {
+        var narrowItDown = this;
+        narrowItDown.found = MenuSearchService.getMatchedMenuItems();
+        console.log("found: " + narrowItDown.found.length);
+        console.log("found[0]: " + narrowItDown.found[0]);
+
+        narrowItDown.removeItem = function (itemIndex) {
+            console.log('going to remove: ' + narrowItDown.found[itemIndex]);
+        }
+
+    }
+
+    MenuSearchService.$inject = ['$http', 'ApiBasePath'];
+    function MenuSearchService($http, ApiBasePath) {
+        var service = this;
+
+        service.getMatchedMenuItems = function (searchTerm) {
+            return $http({
+                method: "GET",
+                url: (ApiBasePath + '/menu_items.json')
+            })
+                .then(function (result) {
+                    // process result and only keep items that match
+                    var foundItems = result.data;
+                    return foundItems;
+                })
+                .catch(function (error) {
+                    console.log('http service request went wrong');
+                });
+        }
+    }
 })();
